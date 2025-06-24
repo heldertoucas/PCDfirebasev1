b48f309dde782b4252e23ab57faa27f92b68660d
heldertoucas <heldertoucas@gmail.com>
2025-06-24 12:08:34 +0100

    fix(config): Corrige sintaxe no ficheiro next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Ativa a exportação estática para gerar a pasta 'out'
+  output: 'export',
 };
 
 export default nextConfig;
