89f88a7fab92f1c89b632c178771d0ad0960b382
heldertoucas <heldertoucas@gmail.com>
2025-06-24 01:54:51 +0100

    feat: Configuração inicial do projeto Next.js e Firebase

---
+++ src/app/lib/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Ficheiro: src/lib/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Inicializar a Firebase apenas se ainda não tiver sido inicializada
+let firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export default firebaseApp;
